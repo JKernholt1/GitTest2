3e0cc8c4d94ede805d1f95cf2707d1cc0789128a
Jonas Kernholt <kerne7676@gmail.com>
2020-12-15 10:35:59 +0100

    Index method class startup

---
+++ src/main/java/org/example/theIndexClassOrSo.java
@@ -0,0 +1,20 @@
+package se.lexicon.JonasKernholt;
+import java.util.Scanner;
+
+public class theIndexClassOrSo {
+
+
+    public static void main(String[] args) {
+    System.out.println("This program lets you type in numbers into a string, and then asks you which part of it to return.");
+        System.out.println("How many numbers would you like to type in?");
+
+
+  }  //main
+
+    private static String indexOf(  String1) {
+
+    return String1;
+    }
+
+
+}  //class
