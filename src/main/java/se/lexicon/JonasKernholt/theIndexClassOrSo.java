a330165368a333aa20517b791fce9d0842497c50
Jonas Kernholt <kerne7676@gmail.com>
2020-12-15 11:01:41 +0100

    Index method class startup and other programs.

--- src/main/java/se/lexicon/JonasKernholt/theIndexClassOrSo.java
+++ src/main/java/se/lexicon/JonasKernholt/theIndexClassOrSo.java
@@ -6,15 +6,15 @@ public class theIndexClassOrSo {
 
     public static void main(String[] args) {
     System.out.println("This program lets you type in numbers into a string, and then asks you which part of it to return.");
-        System.out.println("How many numbers would you like to type in?");
+    System.out.println("How many numbers would you like to type in?");
 
 
   }  //main
 
-    private static String indexOf(  String1) {
+  //  private static String indexOf(String1[]) {
 
-    return String1;
-    }
+  //return String1[];
+  //  }
 
 
 }  //class
